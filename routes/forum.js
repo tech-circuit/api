b3df1301c2b681d310d664e1245a1c954a94b80f
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-06-18 01:15:30 +0530

    forum post endpoint

--- routes/forum.js
+++ routes/forum.js
@@ -23,6 +23,7 @@ router.get('/', async(req, res) => {
         .then(async(posts) => {
             for (let index = 0; index < posts.length; index++) {
                 responsePost = {
+                    id: posts[index]._id,
                     title: posts[index].title,
                     content: posts[index].content,
                     is_upvoted: false,
@@ -58,8 +59,57 @@ router.get('/', async(req, res) => {
             } else if (sort == 'hottest') {
 
             }
-
         })
+})
+
+router.get('/post/:id', async(req, res) => {
+    let user = await User.findOne({ access_token: req.query.access_token })
+    let post = await Post.findOne({ _id: req.params.id })
+    let response = {
+        success: true,
+        authenticated: false,
+        drafts: 0,
+        post_id: req.params.id,
+        title: post.title,
+        content: post.content,
+        is_upvoted: false,
+        is_saved: false,
+        author: '',
+        date: post.date,
+        comments: []
+    }
+    if (user) {
+        response.authenticated = true
+        let drafts = await Post.find({ is_draft: true, author: user._id })
+        response.drafts = drafts.length
+    }
+    let author = await User.findOne({ _id: post.author })
+    response.author = author.username
+    if (user) {
+        post.upvotes.every(upvote => {
+            if (upvote.user == user._id) {
+                response.is_upvoted = true
+                return false
+            }
+            return true
+        })
+        user.saves.every(save => {
+            if (save == post._id) {
+                response.is_saved = true
+                return false
+            }
+            return true
+        })
+    }
+    let comments = await Comment.find({ details: { type: 'post', id: post._id } }).sort({ date: -1 })
+    for (let i = 0; i < comments.length; i++) {
+        let commentAuthor = await User.findOne({ _id: comments[i].author })
+        comments[i].author_name = commentAuthor.name
+        comments[i].author_username = commentAuthor.username
+        comments[i].pfp_url = commentAuthor.pfp_url
+    }
+    response.comments = comments
+    res.json(response)
 })
 
 router.post('/new', async(req, res) => {
