f5f4665637239d8f8b523966ac9d4a045638c412
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-09-13 18:16:06 +0530

    mailing list email check

--- routes/mailingList.js
+++ routes/mailingList.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const emailCheck = require('email-validator')
 const MailingListEmail = require('../models/MailingListEmail')
 
 router.post('/subscribe', async(req, res) => {
-    let mailingListEmail = new MailingListEmail({
-        email: req.body.email,
-        date: Date.now()
-    })
-    try {
-        await mailingListEmail.save()
-        res.json({ success: true })
-    } catch (err) {
-        res.json({ success: false, message: err })
+    let check = await emailCheck.validate(req.body.email)
+    if (check) {
+        let alreadyExists = await MailingListEmail.findOne({ email: req.body.email })
+        if (alreadyExists) {
+            res.json({ success: false, message: 'Email address already subscribed.' })
+        } else {
+            let mailingListEmail = new MailingListEmail({
+                email: req.body.email,
+                date: Date.now()
+            })
+            try {
+                await mailingListEmail.save()
+                res.json({ success: true })
+            } catch (err) {
+                res.json({ success: false, message: err })
+            }
+        }
+    } else {
+        res.json({ success: false, message: 'Email address doesn\'t exist.' })
     }
 });
 
