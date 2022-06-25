5b210a40c52c513ef162cbaf97b110caf74f5613
Anshul Saha <anshulsaha15@gmail.com>
2022-06-25 20:35:01 +0530

    truncate

--- helpers/createNotification.js
+++ helpers/createNotification.js
@@ -5,6 +5,14 @@ const Project = require("../models/Project");
 const Post = require('../models/Post')
 const Event = require("../models/Event");
 
+function truncateText(text) {
+    if (text.length > 19) {
+        return text.substring(0, 16) + "..."
+    } else {
+        return text
+    }
+}
+
 function createNotification(sender, type, typeDetails) {
     // createNotification function details:
     // if type == request, typeDetails is {type: "orgJoin", typeID: orgID}, or {type: "eventOrgHost", typeID: orgID, eventID: eventID}
@@ -22,22 +30,22 @@ function createNotification(sender, type, typeDetails) {
         if (type === "request") {
             let org = await Org.findOne({ _id: typeDetails.typeID });
             if (typeDetails.type === "orgJoin") {
-                notif.meta.description = `${user.given_name} has requested to join '${org.name}' club`
+                notif.meta.description = `${user.given_name} has requested to join '${truncateText(org.name)}' club`
             } else if (typeDetails.type === "eventOrgHost") {
                 let event = await Event.findOne({ _id: typeDetails.eventID })
-                notif.meta.description = `${user.given_name} is requesting '${org.name}' to host '${event.name}' event`
+                notif.meta.description = `${user.given_name} is requesting '${truncateText(org.name)}' to host '${truncateText(event.name)}' event`
             }
             notif.meta.img = user.pfp_url
             receivers = org.admins
         } else if (type === "comment") {
             if (typeDetails.type === "project") {
                 let project = await Project.findOne({ _id: typeDetails.typeID })
-                notif.meta.description = `${user.given_name} has commented on your project '${project.title}'`
+                notif.meta.description = `${user.given_name} has commented on your project '${truncateText(project.title)}'`
                 notif.meta.img = project.cover_image
                 receivers = [project.uploader]
             } else if (typeDetails.type === "post") {
                 let post = await Post.findOne({ _id: typeDetails.typeID })
-                notif.meta.description = `${user.given_name} has commented on your post '${post.title}'`
+                notif.meta.description = `${user.given_name} has commented on your post '${truncateText(post.title)}'`
                 notif.meta.img = user.pfp_url
                 receivers = [post.author]
             }
