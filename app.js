186d58db71f02b1f15c3a87665ca22636fd371c3
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2022-08-11 18:52:47 +0530

    add filter category to search

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 const socketIO = require("socket.io");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const indexRouter = require('./routes/index');
-const userRouter = require('./routes/user');
-const workRouter = require('./routes/work');
-const projectRouter = require('./routes/project');
-const imageRouter = require('./routes/image');
-const forumRouter = require('./routes/forum');
-const mailingListRouter = require('./routes/mailingList');
-const orgRouter = require('./routes/org');
-const orgEvent = require('./routes/event');
-const notifs = require('./routes/notifs');
-const authRouter = require('./routes/auth');
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
+const workRouter = require("./routes/work");
+const projectRouter = require("./routes/project");
+const imageRouter = require("./routes/image");
+const forumRouter = require("./routes/forum");
+const mailingListRouter = require("./routes/mailingList");
+const orgRouter = require("./routes/org");
+const orgEvent = require("./routes/event");
+const notifs = require("./routes/notifs");
+const authRouter = require("./routes/auth");
 
 const db = process.env.MONGODB_URL;
 
@@ -28,41 +28,46 @@ const server = app.listen(port, (err) => {
     if (err) throw err;
 });
 
-const io = socketIO(server, { cors: true, origins: '*:*' });
+const io = socketIO(server, { cors: true, origins: "*:*" });
 
-mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
-    .then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.log(err));
-mongoose.set('useFindAndModify', false);
+mongoose
+    .connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+mongoose.set("useFindAndModify", false);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // settings
-app.use(express.json({ limit: '50mb', extended: true }));
-app.use(express.urlencoded({ limit: '50mb', extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/user', userRouter);
-app.use('/work', workRouter);
-app.use('/project', projectRouter);
-app.use('/image', imageRouter);
-app.use('/forum', forumRouter);
-app.use('/ml', mailingListRouter);
-app.use('/org', orgRouter);
-app.use('/event', orgEvent);
-app.use('/notifs', notifs);
-app.use('/auth', authRouter);
+app.use("/", indexRouter);
+app.use("/user", userRouter);
+app.use("/work", workRouter);
+app.use("/project", projectRouter);
+app.use("/image", imageRouter);
+app.use("/forum", forumRouter);
+app.use("/ml", mailingListRouter);
+app.use("/org", orgRouter);
+app.use("/event", orgEvent);
+app.use("/notifs", notifs);
+app.use("/auth", authRouter);
 
-io.on('connection', socket=>{
-    socket.on('notif', receivers => {
-        console.log(receivers)
-        receivers.forEach(id => {
-            io.emit('notif', id)
-        })
-    })
-})
+io.on("connection", (socket) => {
+    socket.on("notif", (receivers) => {
+        console.log(receivers);
+        receivers.forEach((id) => {
+            io.emit("notif", id);
+        });
+    });
+});
 
 module.exports = app;
--- routes/project.js
+++ routes/project.js
@@ -2,19 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
 const User = require("../models/User");
-// const fields = require('../data/fields')
+const fields = require("../data/fields");
 
-router.get("/", async(req, res) => {
+router.get("/", async (req, res) => {
     try {
         const projects = await Project.find().sort({ createdAt: -1 });
         res.json({ success: true, projects: projects });
     } catch (err) {
         console.log(err);
-        res.json({ success: false, error: err })
+        res.json({ success: false, error: err });
     }
 });
 
-router.get("user/:user", async(req, res) => {
+router.get("/fields", (_, res) => {
+    res.json(fields);
+});
+
+router.get("user/:user", async (req, res) => {
     try {
         const user = await User.findOne({
             _id: req.params.user,
@@ -29,7 +33,7 @@ router.get("user/:user", async(req, res) => {
     }
 });
 
-router.post("/add", async(req, res) => {
+router.post("/add", async (req, res) => {
     const user = await User.findOne({ access_token: req.query.access_token });
     const {
         title,
@@ -73,19 +77,22 @@ router.post("/add", async(req, res) => {
     }
 });
 
-router.put("/edit/:id", async(req, res) => {
+router.put("/edit/:id", async (req, res) => {
     try {
         const user = await User.findOne({
             access_token: req.query.access_token,
         });
-        await Project.updateOne({ _id: req.params.id, uploader: user._id }, { $set: req.body });
+        await Project.updateOne(
+            { _id: req.params.id, uploader: user._id },
+            { $set: req.body }
+        );
         res.json({ success: true });
     } catch (err) {
         res.json({ success: false, error: err });
     }
 });
 
-router.get("/getForEdit/:id", async(req, res) => {
+router.get("/getForEdit/:id", async (req, res) => {
     try {
         const user = await User.findOne({
             access_token: req.query.access_token,
@@ -100,7 +107,7 @@ router.get("/getForEdit/:id", async(req, res) => {
     }
 });
 
-router.delete("/delete/:id", async(req, res) => {
+router.delete("/delete/:id", async (req, res) => {
     try {
         const user = await User.findOne({
             access_token: req.query.access_token,
@@ -112,8 +119,19 @@ router.delete("/delete/:id", async(req, res) => {
     }
 });
 
-router.post("/search", async(req, res) => {
+router.post("/search", async (req, res) => {
     try {
+        const field = req.body.field;
+
+        if (field) {
+            const projects = await Project.find({
+                fields: req.body.field,
+                $text: { $search: req.body.search },
+            });
+
+            return res.json({ success: true, projects: projects });
+        }
+
         const projects = await Project.find({
             $text: { $search: req.body.search },
         });
@@ -123,7 +141,7 @@ router.post("/search", async(req, res) => {
     }
 });
 
-router.post("/field", async(req, res) => {
+router.post("/field", async (req, res) => {
     try {
         const projects = await Project.find({ fields: req.body.field });
         return res.json({ success: true, projects: projects });
@@ -132,7 +150,7 @@ router.post("/field", async(req, res) => {
     }
 });
 
-router.get("/:id", async(req, res) => {
+router.get("/:id", async (req, res) => {
     try {
         const project = await Project.findOne({
             _id: req.params.id,
